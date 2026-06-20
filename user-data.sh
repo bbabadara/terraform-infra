93014b1d6fa85aa2396831e4bb8913518ecd8bb8
Badara <bbabadara@gmail.com>
2026-06-20 14:52:02 +0000

    tes

--- user-data.sh
+++ user-data.sh
@@ -6,6 +6,8 @@ apt install docker.io -y
 systemctl enable docker
 systemctl start docker
 
+usermod -aG docker ubuntu
+
 curl -SL https://github.com/docker/compose/releases/download/v2.39.1/docker-compose-linux-x86_64 \
   -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
